6340c9a5b0a998f505707a1eb1828d990c664618
Dhinakaran MS <168914080+dhinakaran311@users.noreply.github.com>
2025-12-12 21:51:43 +0530

    kthLargestElement

--- Binary_Heap/findKthLargest.java
+++ Binary_Heap/findKthLargest.java
@@ -1,12 +1,28 @@
-class Solution {
-    public int findKthLargest(int[] nums, int k) {
+import java.util.*;
+
+public class findKthLargest {
+    public static int findKthLargest(int[] a, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i:nums){
+        for (int i : a) {
             pq.add(i);
-            if(pq.size()>k){
-                pq.poll();
-            }
+            if (pq.size() > k) pq.poll();
         }
         return pq.peek();
-}
+    }
+
+    public static void main(String[] args) {
+        int[][] t = {
+            {3,2,1,5,6,4},
+            {3,2,3,1,2,4,5,5,6},
+            {1},
+            {7,10,4,3,20,15},
+            {5,5,5,5,5}
+        };
+
+        int[] k = {2,4,1,3,1};
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(findKthLargest(t[i], k[i]));
+        }
+    }
 }
