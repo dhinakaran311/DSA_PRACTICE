b80ff4e0072afb4525d33f86d2026f474d17ff54
Dhinakaran MS <168914080+dhinakaran311@users.noreply.github.com>
2025-12-17 10:24:34 +0530

    KthLargestInStreamData

---
+++ Binary_Heap/KthLargestInStreamData.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class KthLargestInStreamData {
+    int k;
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public KthLargestInStreamData(int k, int[] nums) {
+        this.k = k;
+        for (int v : nums) {
+            pq.add(v);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        pq.add(val);
+        if (pq.size() > k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+
+    // -------- MAIN METHOD FOR LOCAL RUN --------
+    public static void main(String[] args) {
+
+        int k = 3;
+        int[] nums = {4, 5, 8, 2};
+
+        KthLargest obj = new KthLargest(k, nums);
+
+        System.out.println(obj.add(3));   // 4
+        System.out.println(obj.add(5));   // 5
+        System.out.println(obj.add(10));  // 5
+        System.out.println(obj.add(9));   // 8
+        System.out.println(obj.add(4));   // 8
+    }
+}
